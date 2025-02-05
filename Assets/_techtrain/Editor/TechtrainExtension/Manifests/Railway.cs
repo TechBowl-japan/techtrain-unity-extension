1f3269120a3dc1adba83a7df9e802b328f5936ab
XPA <xpadev@gmail.com>
2025-02-05 17:45:46 +0900

    feat: manifest model

---
+++ Assets/_techtrain/Editor/TechtrainExtension/Manifests/Railway.cs
@@ -0,0 +1,12 @@
+#nullable enable
+
+using System.Collections.Generic;
+
+namespace TechtrainExtension.Manifests
+{
+    public sealed class Railway
+    {
+        public int railwayId { get; set; }
+        public Dictionary<string, string> railway { get; set; }
+    }
+}
---
+++ Assets/_techtrain/Editor/TechtrainExtension/Manifests/Station.cs
@@ -0,0 +1,29 @@
+#nullable enable
+
+using System.Collections.Generic;
+
+namespace TechtrainExtension.Manifests
+{
+    public sealed class Station
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public List<StationPrepare>? prepare { get; set; }
+        public List<StationTest> tests { get; set; }
+    }
+
+    public sealed class StationPrepare
+    {
+        public string command { get; set; }
+        public bool background { get; set; }
+    }
+
+    public sealed class StationTest
+    {
+        public string type { get; set; }
+        public string? title { get; set; }
+        public string command { get; set; }
+        public List<string> args { get; set; }
+        public bool shell { get; set; }
+    }
+}
